ce00724cd12f1c32d57fb11cf034eba611c97cf9
Thanassis Tsiodras <ttsiodras@gmail.com>
2015-09-20 16:55:41 +0200

    Set upper limits

--- css_maker.py
+++ css_maker.py
@@ -4,18 +4,23 @@
 
 template = open("html/matrix.css.template").read()
 o = open("html/matrix.css", "w")
-for viewport_size in xrange(100, 5000, 100):
+for viewport_size in xrange(100, 900, 100):
     viewport_size_p100 = viewport_size + 99
-    tile_size = int(viewport_size/20.)
+    tile_size = int(viewport_size / 20.)
     tile_size_p5 = tile_size + 5
     tile_size_p4 = tile_size + 4
-    o.write(
-        template % {
-            "viewport_size": viewport_size,
-            "viewport_size_p100": viewport_size_p100,
-            "tile_size": tile_size,
-            "tile_size_p5": tile_size_p5,
-            "tile_size_p4": tile_size_p4
-        }
-    )
-
+    data = {
+        "minwidth": "(min-width: %dpx)" % viewport_size
+    }
+    if viewport_size < 800:
+        data.update({
+            "maxwidth": "and (max-width: %dpx)" % viewport_size_p100
+        })
+    elif viewport_size == 800:
+        data.update({"maxwidth": ""})
+    data.update({
+        "tile_size": tile_size,
+        "tile_size_p5": tile_size_p5,
+        "tile_size_p4": tile_size_p4
+    })
+    o.write(template % data)
